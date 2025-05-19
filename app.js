309639bb394db08334749ecdd6e382209a28a123
Assia456 <assiaidali456@gmail.com>
2025-05-19 11:25:58 +0200

    iu oem

--- app.js
+++ app.js
@@ -8,94 +8,90 @@
 //6: possibilité de rejouer
 
 
-
-
-
-//Créer une fonction afficher 
-//Créer une fonction question suivante
-
 //Créer un tableau réponses et créer un tableau question
 
 let tableauQST = [
 
     {
         question: "Quel est le panneau piste cyclable?",
-        a:"panneau a",
-        b:"panneau b",
-        c:"panneau c",
-        d:"panneau d",
-        r:"a" 
+        a: "panneau a",
+        b: "panneau b",
+        c: "panneau c",
+        d: "panneau d",
+        r: "a"
     },
 
     {
         question: "Le panneau est une obligation de:",
-        a:"Tourner à droite",
-        b:"Tourner à gauche",
-        c:"Faire demitour",
-        d:"Parking",
-        r:"b"
+        a: "Tourner à droite",
+        b: "Tourner à gauche",
+        c: "Faire demi tour",
+        d: "Parking",
+        r: "b"
     },
 
     {
         question: "Qui est prioritaire dans cette situation ?",
-        a:"moto A",
-        b:"moto b",
-        c:"vehicule c",
-        d:"Il n'y pas de panneau"
+        a: "moto A",
+        b: "moto b",
+        c: "vehicule c",
+        d: "Il n'y pas de panneau"
     },
 
     {
         question: "Sans presance de panneau de vitesse a combien etes vous autorisé a rouler ?",
-        a:"50 km/h",
-        b:"30 km/h",
-        c:"80 km/h",
-        d:"20 km/h",
-        r:"c"
+        a: "50 km/h",
+        b: "30 km/h",
+        c: "80 km/h",
+        d: "20 km/h",
+        r: "c"
     },
 
     {
         question: "Quel est le mot manquant sur le panneau ?",
         a: "Gendarmerie",
-        b:"Garderie",
-        c:"Prison",
-        d:"Air de jeu",
-        r:"a"
-        
+        b: "Garderie",
+        c: "Prison",
+        d: "Air de jeu",
+        r: "a"
+
     }
 ]
 
-//Créer une variable qui s'appelle réponse qui va stocker la reponse utilisateur via un propmt " question?"
 
+let compter = 0
 
-
-
-//Créer une boucle qui compare la reponse de l'utilisateur a la reponse attendue
-
-//Role:
-//Parametre:
-//Return:
-
-let compter=0
+//Role:Afficher le quizz
+//Parametre:Le tableauQST
+//Return:Rien
 
 function Quizz(question) {
 
+    //Créer une boucle qui compare la reponse de l'utilisateur a la reponse attendue
+
     tableauQST.forEach(question => {
+        
+        //Affiche une boîte de dialogue, éventuellement avec un message, qui invite l'utilisateur à saisir ça réponse.
+
         let reponse = prompt(`${question.question} : \n
             a: ${question.a} \n
             b: ${question.b}\n
             c:${question.c}\n
             d:${question.d}
             `)
+
+        //Si la réponse de l'utilisateur est égale a la réponse de l'ordinateur alors : "Bonne réponse!"
         if (reponse === question.r) {
-            console.log("Bonne réponse")
+            console.log("Bonne réponse !")
             compter++
+            //Si la réponse de l'utilisateur n'est pas égale a la réponse de l'ordinateur alors: "Presque ça..."
         } else {
             console.log("Presque ça...")
         }
     })
 
-    alert(`Ton score est de ${compter}`)
+
 }
 
 Quizz();
-
+alert(`Ton score est de ${compter}`)
